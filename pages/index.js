2248a60be38709c9602c0a15f5931cee5aab55d5
Gegerout <72309553+Gegerout@users.noreply.github.com>
2022-03-08 21:22:59 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [inputText, setInputText] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ inputText: inputText }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    setInputText("");
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Grammar Corrector</title>
+        <link rel="icon" href="/hat.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/hat.png" className={styles.icon} />
+        <h3>Grammar Corrector</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="inputText"
+            placeholder="Enter sentence"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          <input type="submit" value="Correct grammar" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
